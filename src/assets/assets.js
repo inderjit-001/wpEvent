92302b326d72a4fd79a58547af384745357261f0
Inderjit Singh <inderj1998@gmail.com>
2025-09-10 15:05:36 +0530

    images setup

---
+++ src/assets/assets.js
@@ -0,0 +1,17 @@
+import about3 from "./about-img-3.jpg"
+import aboutimg from "./about-img.jpg"
+import aboutVideoBtn from "./about-video-btn-bg.jpg"
+import logoWhite from "./logo-white.png"
+import reviewerOne from "/reviewer-1.png"
+import reviewerTwo from "/reviewer-2.png"
+import reviewerThree from "/reviewer-3.png"
+
+export const assets = {
+    about3,
+    aboutimg,
+    aboutVideoBtn,
+    logoWhite,
+    reviewerOne,
+    reviewerTwo,
+    reviewerThree,
+}
